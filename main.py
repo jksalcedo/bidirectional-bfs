adc779e08b72645ab880eccff5aba97f82682a17
jksalcedo <jks.create@gmail.com>
2025-10-15 14:16:49 +0000

    Initialize repository

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# 2D array as the gridmap
+grid = [[0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
